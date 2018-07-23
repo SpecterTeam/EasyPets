e8411aba94abed0e0baff8baad965b87c3ac4cff
Fris <22847866+OGFris@users.noreply.github.com>
2018-07-23 02:00:11 +0100

    Update PetsSession.php

--- src/PT/PetsManager/PetsSession.php
+++ src/PT/PetsManager/PetsSession.php
@@ -1,9 +1,19 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Walid
- * Date: 21/05/2018
- * Time: 16:08
+ *     EasyPets  Copyright (C) 2018  SpecterTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace PT\PetsManager;
@@ -16,8 +26,6 @@ use pocketmine\Player;
 
 class PetsSession
 {
-
-
     public static $players = [];
 
     const BIGGEST_PETS_BY_ID = [
