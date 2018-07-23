00ea9ac24334a7be2d81fbd0423095b7dfcdec33
Fris <22847866+OGFris@users.noreply.github.com>
2018-07-23 01:59:28 +0100

    Update Pets.php

--- src/PT/PetsManager/Pets.php
+++ src/PT/PetsManager/Pets.php
@@ -1,16 +1,27 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Walid
- * Date: 21/05/2018
- * Time: 16:08
+ *     EasyPets  Copyright (C) 2018  SpecterTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace PT\PetsManager;
 
 class Pets
 {
 
-    //You can suggest some animals in the list
+    //Create an issue to request a new pet type to add. <https://github.com/SpecterTeam/EasyPets>
 
     public static $pets =
         ["Wolf" => 14,
